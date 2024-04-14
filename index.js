73074f5dd80e935ab0dd44f45b362b0694c49d43
fatush254 <fgalgalo945@gmail.com>
2024-04-14 23:36:01 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+// Define upperCaseCustomerName function
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Define setBestCustomer function
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Define overwriteBestCustomer function
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Define changeLeastFavoriteCustomer function
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'someone else';
+  leastFavoriteCustomer = 'bobby'; // This line will throw an error due to trying to reassign a constant
+}
+
+// Export the functions
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
